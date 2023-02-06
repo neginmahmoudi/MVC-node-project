ddc3acf321390433ff20536d2e3427854af8cd7f
Negin Mahmoudi <negin.mahmoudii76@gmail.com>
2023-02-06 11:12:32 +0100

    Add controllers

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -18,30 +21,34 @@ app.use(express.json());
 //serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
+// routes
+app.use('/', require('./routes/root'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, 'index.html'); //302 by default
-});
-// Route handlers
-app.get(
-  '/hello(.html)?',
-  (req, res, next) => {
-    console.log('attempted to run the file');
-    next();
-  },
-  (req, res) => {
-    res.send('hello world!');
-  },
-);
+// app.get('^/$|/index(.html)?', (req, res) => {
+//   // res.sendFile('./views/index.html', { root: __dirname });
+//   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// });
+// app.get('/new-page(.html)?', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+// });
 
-pp.all('*', (req, res) => {
+// app.get('/old-page(.html)?', (req, res) => {
+//   res.redirect(301, 'index.html'); //302 by default
+// });
+// // Route handlers
+// app.get(
+//   '/hello(.html)?',
+//   (req, res, next) => {
+//     console.log('attempted to run the file');
+//     next();
+//   },
+//   (req, res) => {
+//     res.send('hello world!');
+//   },
+// );
+
+app.all('*', (req, res) => {
   res.status(404);
   if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
